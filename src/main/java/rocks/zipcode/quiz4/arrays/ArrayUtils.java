847025d83cef5bc0b56b3b608aaa3c91f74f1900
Dandridge Cole <dandridgecole@yahoo.com>
2019-07-16 09:44:04 -0400

    ArrayUtils passing all tests

--- src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
+++ src/main/java/rocks/zipcode/quiz4/arrays/ArrayUtils.java
@@ -1,22 +1,27 @@
 package rocks.zipcode.quiz4.arrays;
 
+import java.util.Arrays;
+
 /**
  * @author leon on 01/01/2019.
  */
 public class ArrayUtils {
     public static String getMiddleElement(String[] values) {
-        return null;
+        return values[values.length/2];
     }
 
     public static String[] removeMiddleElement(String[] values) {
-        return null;
+        String[] answer = new String[values.length-1];
+        System.arraycopy(values, 0, answer, 0, values.length/2);
+        System.arraycopy(values, (values.length/2)+1,answer,(values.length/2),values.length-values.length/2-1);
+        return answer;
     }
 
     public static String getLastElement(String[] values) {
-        return null;
+        return values[values.length-1];
     }
 
     public static String[] removeLastElement(String[] values) {
-        return null;
+        return Arrays.copyOf(values,values.length-1);
     }
 }
