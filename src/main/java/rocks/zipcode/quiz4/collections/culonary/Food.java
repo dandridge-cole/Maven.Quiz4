e1381b16827cc911bf7434b0907b9c8f4a420ea4
Dandridge Cole <dandridgecole@yahoo.com>
2019-07-16 10:19:55 -0400

    Polymorphism: account & bank account passing all tests

--- src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
+++ src/main/java/rocks/zipcode/quiz4/collections/culonary/Food.java
@@ -1,5 +1,6 @@
 package rocks.zipcode.quiz4.collections.culonary;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -7,6 +8,8 @@ import java.util.Map;
  * @author leon on 27/12/2018.
  */
 public class Food {
+    private Map<Class<? extends Spice>,Integer> spices;
+
     public List<Spice> getAllSpices() {
         return null;
     }
--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/BankAccount.java
@@ -3,7 +3,26 @@ package rocks.zipcode.quiz4.objectorientation.account;
 /**
  * @author leon on 27/12/2018.
  */
-public class BankAccount {
+public class BankAccount extends Account implements Transactable {
+    private Double balance;
     public void setBalance(Double val) {
+        this.balance=val;
+    }
+
+    @Override
+    public void deposit(Double amountToIncreaseBy) throws IllegalArgumentException{
+        if(amountToIncreaseBy<0)throw new IllegalArgumentException();
+        this.balance+=amountToIncreaseBy;
+    }
+
+    @Override
+    public void withdrawal(Double amountToDecreaseBy) throws IllegalArgumentException{
+        if(amountToDecreaseBy<0||amountToDecreaseBy>this.balance)throw new IllegalArgumentException();
+        this.balance-=amountToDecreaseBy;
+    }
+
+    @Override
+    public Double getBalance() {
+        return this.balance;
     }
 }
