38ccc7007cb19e8b53f38e5b927a79eda57686f6
Dandridge Cole <dandridgecole@yahoo.com>
2019-07-16 09:08:53 -0400

    calculator passing all tests

--- src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
@@ -5,31 +5,38 @@ package rocks.zipcode.quiz4.fundamentals;
  */
 public class Calculator {
     public static Double squareRoot(Double value) {
-        return null;
+        return Math.sqrt(value);
     }
 
     public static Double square(Double value) {
-        return null;
+        return value*value;
     }
 
     public static Double[] squareRoots(Double... value) {
-        return null;
+        Double[] roots = new Double[value.length];
+        for(int i=0; i<value.length;i++){
+            roots[i]=squareRoot(value[i]);
+        }
+        return roots;
     }
 
     public static Double[] squares(Double... values) {
-        return null;
+        Double[] squares = new Double[values.length];
+        for(int i=0; i<values.length;i++){
+            squares[i]=square(values[i]);
+        }
+        return squares;
     }
 
     public static Double add(Double value1, Double value2) {
-        return null;
+        return value1+value2;
     }
 
     public static Double subtract(Double value1, Double value2) {
-        return null;
+        return value1-value2;
     }
 
-
     public static Double divide(Double divisor, Double dividend) {
-        return null;
+        return divisor/dividend;
     }
 }
