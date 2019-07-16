b39ac6b84ee7c3d131d5c909915203fb2fed4ec9
Dandridge Cole <dandridgecole@yahoo.com>
2019-07-16 10:25:24 -0400

    Polymorphism: bank passing all tests

--- src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
@@ -1,17 +1,26 @@
 package rocks.zipcode.quiz4.objectorientation.account;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 27/12/2018.
  */
 public class Bank {
+    List<BankAccount> bAccountList = new ArrayList<>();
+
     public BankAccount removeBankAccountByIndex(Integer indexNumber) {
-        return null;
+        BankAccount removed = this.bAccountList.get(indexNumber);
+        this.bAccountList.remove(removed);
+        return removed;
     }
 
     public void addBankAccount(BankAccount bankAccount) {
+        this.bAccountList.add(bankAccount);
     }
 
     public Boolean containsBankAccount(BankAccount bankAccount) {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        return this.bAccountList.contains(bankAccount);
+        //throw new UnsupportedOperationException("Method not yet implemented");
     }
 }
