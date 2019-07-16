c614e8131d9c3282f4ae5da90362967f6fb55432
Dandridge Cole <dandridgecole@yahoo.com>
2019-07-16 09:51:53 -0400

    wordcounter passing all tests

--- src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
+++ src/main/java/rocks/zipcode/quiz4/collections/WordCounter.java
@@ -1,12 +1,20 @@
 package rocks.zipcode.quiz4.collections;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordCounter {
+    private String[] words;
     public WordCounter(String... strings) {
+        this.words=strings;
     }
 
     public Map<String, Integer> getWordCountMap() {
-        return null;
+        Map<String,Integer> map=new HashMap<>();
+        for(String s:this.words){
+            if(map.containsKey(s))map.replace(s,map.get(s)+1);
+            else map.put(s,1);
+        }
+        return map;
     }
 }
